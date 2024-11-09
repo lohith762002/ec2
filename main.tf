6df1c2b56b6abed7fc90a86c2ddaf5fb0c5c548d
Lohithkumar <123312695+lohith762002@users.noreply.github.com>
2024-11-09 10:21:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,16 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
+count = 4
 ami = "ami-04a37924ffe27da53"
-instance_type = "t2.micro"
+instance_type = "t2.medium"
+vpc_security_groups_ids = ["sg-041e08f4412b38651"]
 
  tags = {
-    Name = "possible"
+    Name = var.instance_names[count.index]
   }
+}
+
+variable "instance_names" {
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2"]
 }
