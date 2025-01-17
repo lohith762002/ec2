fce7fb9f3066817213f754877d512b6a659281e0
Lohithkumar <123312695+lohith762002@users.noreply.github.com>
2025-01-17 18:05:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 4
 ami = "ami-04a37924ffe27da53"
 instance_type = "t2.medium"
-vpc_security_groups_ids = ["sg-041e08f4412b38651"]
+vpc_security_groups_ids = ["sg-03b3778957540ea86"]
 
  tags = {
     Name = var.instance_names[count.index]
