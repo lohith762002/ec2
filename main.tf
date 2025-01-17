7d56cfb53a1a5bc5d241783dc80e80c2a2fed0d0
Lohithkumar <123312695+lohith762002@users.noreply.github.com>
2025-01-17 18:19:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-04a37924ffe27da53"
+ami = "ami-0454e52560c7f5c55"
 instance_type = "t2.medium"
 vpc_security_group_ids = ["sg-03b3778957540ea86"]
 
